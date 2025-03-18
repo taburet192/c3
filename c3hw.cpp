e5d34931d55730f3b793fdc32aa73069eee7baa5
taburet192 <romaprudnik101@gmail.com>
2025-03-18 19:24:16 +0300

    Add files via upload

---
+++ c3hw.cpp
@@ -0,0 +1,96 @@
+#include "iostream"
+
+
+
+
+bool iseven(int a)
+{
+	int c = a % 2;
+	return c;
+
+
+
+}
+
+int main()
+{
+	int a;
+	std::cin >> a
+	if(iseven(a)==0)
+	{
+	std::cout << "a is even \n" << \n;
+	}
+	else
+	{
+	std::cout << "a is odd \n" << \n;
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//пасхалочка
